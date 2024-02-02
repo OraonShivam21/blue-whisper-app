636937eec1ec7773640b6a31ec57c7b0ce3219e0
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-02 21:21:13 +0530

    created user model

---
+++ server/models/user.model.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: `https://via.placeholder.com/200x200.png`,
+        localPath: "",
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: ["ADMIN", "USER"],
+      default: "USER",
+      required: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    loginType: {
+      type: String,
+      enum: ["GOOGLE", "GITHUB", "EMAIL_PASSWORD"],
+      default: "EMAIL_PASSWORD",
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
