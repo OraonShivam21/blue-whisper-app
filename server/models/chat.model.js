265fecb0ccd33fb0d018e9f5439f1a9f8ef22e5c
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-02 20:51:09 +0530

    created chat model

---
+++ server/models/chat.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    isGroupChat: {
+      type: Boolean,
+      required: true,
+    },
+    lastMessage: {
+      type: Schema.Types.ObjectId,
+      ref: "ChatMessage",
+    },
+    participants: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    admin: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Chat = mongoose.model("Chat", chatSchema);
