9abc40f9d8c24c80b1e97a792edbcd78845709d2
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-03 14:12:10 +0530

    created api-error util

--- server/utils/ApiError.js
+++ server/utils/ApiError.js
@@ -1,1 +1,25 @@
-const errorHandler = require("../middlewares/error.middlewares")
+const errorHandler = require("../middlewares/error.middlewares");
+
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+module.exports = ApiError;
