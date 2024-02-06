218df83482b09b8e9aba0ad2a24eaafa897f4bdc
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-06 16:07:28 +0530

    updated user controller

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -72,4 +72,68 @@ const registerUser = asyncHandler(async (req, res) => {
       )}/api/users/verify-email/${unHashedToken}`
     ),
   });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken -emailVerificationToken -emailVerificationExpiry"
+  );
+
+  if (!createdUser)
+    throw new ApiError(500, "Something went wrong while registering the user");
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(
+        200,
+        { user: createdUser },
+        "Users registered successfully and verification email has been sent on your email"
+      )
+    );
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, username, password } = req.body;
+
+  if (!username && !email)
+    throw new ApiError(400, "Username or email is required");
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) throw new ApiError(404, "User does not exists");
+
+  // if user is registered with oauth methods, we will ask them to use the same method as registered
+  // this shows that if user is registered with methods other than email password, they will not be able to login with password, which makes password field redundant for the SSO
+  if (user.loginType !== "EMAIL_PASSWORD")
+    throw new ApiError(
+      400,
+      `You have previously registered using ${user.loginType?.toLowerCase()}. Please use the ${user.loginType?.toLowerCase()} login option to access your account.`
+    );
+
+  // compare the incoming password with hashed password
+  const isPasswordValid = await user.isPasswordValid(password);
+
+  if (!isPasswordValid) throw new ApiError(401, "Invalid user credentials");
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  // get the user document ignoring the password and refreshToken field
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken -emailVerificationToken -emailVerificationExpiry"
+  );
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken)
+    .cookie("refreshToken", refreshToken)
+    .json(
+      new ApiResponse(
+        200,
+        { user: loggedInUser, accessToken, refreshToken },
+        "User logged in successfully"
+      )
+    );
 });
