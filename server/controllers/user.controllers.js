a16bb73711d8b65bb5b5e3ffa4107e13426eea02
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-06 16:27:12 +0530

    added resend email verification

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -191,4 +191,33 @@ const verifyEmail = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, { isEmailVerified: true }, "Email is verified"));
 });
 
+const resendEmailVerification = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user?._id);
 
+  if (!user) throw new ApiError(404, "User does not exists", []);
+
+  // if email is already verified throw an error
+  if (user.isEmailVerified) throw new ApiError(409, "Email already verified!");
+
+  const { unHashedToken, hashedToken, tokenExpiry } =
+    user.generateTemporaryToken();
+
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationExpiry = tokenExpiry;
+  await user.save({ validateBeforeSave: false });
+
+  await sendEmail({
+    email: user?.email,
+    subject: "Please verify your email",
+    mailgenContent: emailVerificationMailgenContent(
+      user.username,
+      `${req.protocol}://${req.get(
+        "host"
+      )}/api/users/verify-email/${unHashedToken}`
+    ),
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Mail has been sent to your mail ID"));
+});
