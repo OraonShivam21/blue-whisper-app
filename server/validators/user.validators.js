d24d9ec9e1b53b4b4a37c1b8bdae7000ef37f638
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-07 16:27:27 +0530

    added user validator

---
+++ server/validators/user.validators.js
@@ -0,0 +1,72 @@
+const { body } = require("express-validator");
+
+const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("Username is required")
+      .isLowercase()
+      .withMessage("Username must be lowercase")
+      .isLength({ min: 3 })
+      .withMessage("Username must be at least 3 characters long"),
+    body("password").trim().notEmpty().withMessage("Password is required"),
+    body("role")
+      .optional()
+      .isIn(["ADMIN", "USER"])
+      .withMessage("Invalid user role"),
+  ];
+};
+
+const userLoginValidator = () => {
+  return [
+    body("email").optional().isEmail().withMessage("Email is invalid"),
+    body("username").optional(),
+    body("password").notEmpty().withMessage("Password is required"),
+  ];
+};
+
+const userChangeCurrentPasswordValidator = () => {
+  return [
+    body("oldPassword").notEmpty().withMessage("Old password is required"),
+    body("newPassword").notEmpty().withMessage("New password is required"),
+  ];
+};
+
+const userForgotPasswordValidator = () => {
+  return [
+    body("email")
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+  ];
+};
+
+const userResetForgottenPasswordValidator = () => {
+  return [body("newPassword").notEmpty().withMessage("Password is required")];
+};
+
+const userAssignRoleValidator = () => {
+  return [
+    body("role")
+      .optional()
+      .isIn(["ADMIN", "USER"])
+      .withMessage("Invalid user role"),
+  ];
+};
+
+module.exports = {
+  userAssignRoleValidator,
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userLoginValidator,
+  userRegisterValidator,
+  userResetForgottenPasswordValidator,
+};
