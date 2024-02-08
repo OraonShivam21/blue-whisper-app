19e6ce228c06346ddcc2b9664b215a09f52d6f14
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-08 20:23:03 +0530

    updated with required middlewares

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
 const connection = require("./connection.js");
 const userRoute = require("./routes/user.routes.js");
 const chatRoute = require("./routes/chat.routes.js");
 const messageRoute = require("./routes/message.routes.js");
+const errorHandler = require("./middlewares/error.middlewares.js");
 require("dotenv").config({
   path: "./.env",
 });
@@ -39,6 +42,24 @@ app.use(
 
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public")); // configure static file to save images locally
+
+app.use(cookieParser());
+
+// required for passport
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/api/users", userRoute);
 
 app.use("/api/chats", chatRoute);
@@ -52,6 +73,8 @@ app.get("/api", (req, res) => {
     res.status(400).json({ error });
   }
 });
+
+app.use(errorHandler);
 
 httpServer.listen(port, async () => {
   try {
