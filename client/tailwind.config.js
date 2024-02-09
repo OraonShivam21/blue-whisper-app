a94df241ff54397d2b9d22479d69f79f97d53df6
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-09 13:31:26 +0530

    added dev dependencies

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js, jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6b8afd",
+        secondary: "#2e333d",
+        dark: "#212328",
+        danger: "#eb3330",
+        success: "#4aac68",
+      },
+    },
+  },
+  plugins: [],
+};
