d34bb119de1c729062c2857386a836441748f7be
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-26 21:32:14 +0530

    started working on auth context

---
+++ client/src/context/AuthContext.tsx
@@ -0,0 +1,21 @@
+import { createContext } from "react";
+import { UserInterface } from "../interfaces/user";
+
+// Create a context to manage authentication-related data and functions
+const AuthContext = createContext<{
+  user: UserInterface | null;
+  token: string | null;
+  login: (data: { username: string; password: string }) => Promise<void>;
+  register: (data: {
+    email: string;
+    username: string;
+    password: string;
+  }) => Promise<void>;
+  logout: () => Promise<void>;
+}>({
+  user: null,
+  token: null,
+  login: async () => {},
+  register: async () => {},
+  logout: async () => {},
+});
