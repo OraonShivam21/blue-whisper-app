57e86d98f55366bae1d30194c0d34e369ebf0fba
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-27 00:22:49 +0530

    working on AuthProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import {AuthProvider} from "./context/AuthContext.tsx";
-import { BrowserRouter } from "react-router-dom";}
-import {SocketProvider} from "./context/SocketContext.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { SocketProvider } from "./context/SocketContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
