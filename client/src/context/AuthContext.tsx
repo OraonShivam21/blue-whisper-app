da2f7c764aeeba4fe567e072dd661f14e4bcade7
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-27 00:21:53 +0530

    creating auth context

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,5 +1,7 @@
-import { createContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { UserInterface } from "../interfaces/user";
+import { useNavigate } from "react-router-dom";
+import { LocalStorage, requestHandler } from "../utils";
 
 // Create a context to manage authentication-related data and functions
 const AuthContext = createContext<{
@@ -19,3 +21,34 @@ const AuthContext = createContext<{
   register: async () => {},
   logout: async () => {},
 });
+
+// Create a hook to access the AuthContext
+const useAuth = () => useContext(AuthContext);
+
+// Create a component that provides authentication-related data and functions
+const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState<UserInterface | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+
+  const navigate = useNavigate();
+
+  // Function to handle user login
+  const login = async (data: { username: string; password: string }) => {
+    await requestHandler(
+      async () => await loginUser(data),
+      setIsLoading,
+      (res) => {
+        const { data } = res;
+        setUser(data.user);
+        setToken(data.accessToken);
+        LocalStorage.set("user", data.user);
+        LocalStorage.set("token", data.accessToken);
+        navigate("/chat");
+      },
+      alert
+    );
+  };
+};
