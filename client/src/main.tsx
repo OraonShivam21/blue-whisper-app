a092ea593d122e2cb1117fff6df14d612caf88b8
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-02-26 21:26:36 +0530

    created main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import {AuthProvider} from "./context/AuthContext.tsx";
+import { BrowserRouter } from "react-router-dom";}
+import {SocketProvider} from "./context/SocketContext.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <AuthProvider>
+        <SocketProvider>
+          <App />
+        </SocketProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
