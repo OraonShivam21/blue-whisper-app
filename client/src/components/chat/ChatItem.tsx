b2ca5f6ab7ce4092b485b5c42a15608660b3e4ba
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-03-10 15:17:10 +0530

    added group chat details modal

--- client/src/components/chat/ChatItem.tsx
+++ client/src/components/chat/ChatItem.tsx
@@ -1,6 +1,12 @@
+import React, { useState } from "react";
 import {
   EllipsisVerticalIcon,
   PaperClipIcon,
-  TrashIcon
-} from "@heroicons/react/16/solid";
+  TrashIcon,
+} from "@heroicons/react/20/solid";
 import { InformationCircleIcon } from "@heroicons/react/24/outline";
+import moment from "moment";
+import { deleteOneOnOneChat } from "../../api";
+import { useAuth } from "../../context/AuthContext";
+import { ChatListItemInterface } from "../../interfaces/chat";
+import { classNames, getChatObjectMetaData, requestHandler } from "../../utils";
